1dd24cd4ded28e03defe76d02fe88f06644f3d69
niemingzhi <niemingzhi@sinsoft.com.cn>
2022-09-08 11:04:24 +0800

    pagehelp引入增加分页（前端还没写）

--- src/main/java/com/nmz/concretestatistics/controller/BusinessQueryController.java
+++ src/main/java/com/nmz/concretestatistics/controller/BusinessQueryController.java
@@ -1,6 +1,8 @@
 package com.nmz.concretestatistics.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.nmz.concretestatistics.mapper.BusinessDetialsMapper;
 import com.nmz.concretestatistics.povo.BusinessDetials;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BusinessQueryController {
@@ -31,10 +34,14 @@ public class BusinessQueryController {
         BusinessDetials businessDetials = new BusinessDetials();
         businessDetials.setBusiness_name(comp_name);
         businessDetials.setBusiness_date(add_date);
+        PageHelper.startPage(1 , 1);
         List<BusinessDetials> businessDetialslist = businessDetialsMapper.queryAll(businessDetials);
+        //根据查询的数据列表，得到分页的结果对象
+        PageInfo<BusinessDetials> pageList = new PageInfo<BusinessDetials>(businessDetialslist);
+        List<BusinessDetials> datas = pageList.getList();
         /*防止中文乱码*/
         response.setCharacterEncoding("utf-8");
-        String json = JSONObject.toJSONString(businessDetialslist);
+        String json = JSONObject.toJSONString(datas);
         response.getWriter().println(json);
     }
 }
